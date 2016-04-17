d2f314848eb564c332a5002f7eafdd9ded4a2752
Roy Ng <chinkong@gmail.com>
2016-04-17 19:37:35 +0800

    MDEC MOOC JH Week3 Assignment Submission - with comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,12 @@
 
 makeCacheMatrix <- function(x = matrix()) 
 {
-  m <- NULL
-  set<-function(y)
+  # Provides a default for m if cacheSolve has not yet been used when the global cache is run for the first time
+  m <- NULL 
+  set <- function(y) 
   {
-    x <<- y
-    m<<-NULL
+    x <<- y # Cache the input matrix by setting to the global environment variable
+    m <<- NULL
   }
   
   get <- function() x
@@ -25,14 +26,15 @@ makeCacheMatrix <- function(x = matrix())
 cacheSolve <- function(x, ...) 
 {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getMatrix()
+  m <- x$getMatrix() # Accessing the variable x that is in the global environment (as opposed to the calling environment)
   
   if(!is.null(m))
   {
     message("getting cached data")
     return(m)
   }
-  
+
+  # If the cache returns nothing, proceed to solve the inverse of the matrix  
   matrix<-x$get()
   m <- solve(matrix, ...)
   
