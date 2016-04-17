6c22d44127f275345708c1c98edbe0e76fb61c7a
Roy Ng <chinkong@gmail.com>
2016-04-17 19:29:55 +0800

    MDEC MOOC JH Week3 Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <- NULL
+  set<-function(y)
+  {
+    x <<- y
+    m<<-NULL
+  }
+  
+  get <- function() x
+  setMatrix <- function(solve) m <<- solve
+  getMatrix <- function() m
+  
+  list(set=set, get=get, setMatrix = setMatrix, getMatrix = getMatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrix()
+  
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  
+  matrix<-x$get()
+  m <- solve(matrix, ...)
+  
+  x$setMatrix(m)
+  
+  m
 }
